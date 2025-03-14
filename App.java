cd1b245e06b8ca0c9f3c3b2238c1c99bcd34cce0
VictorDiegoGarridoSanchez <garridod563@gmail.com>
2025-03-14 09:39:46 +0100

    Modificando App.java y README.md

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Victor Diego Garrido Sanchez");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("IES San Andres");
+    System.out.println("León");
   }
   
 }
